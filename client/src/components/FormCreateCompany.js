569259d5c045d57177c5a3eedc57a30343b99453
maenggar <maenggar.map@gmail.com>
2020-05-01 13:29:18 +0700

    adding yup for  the form

--- client/src/components/FormCreateCompany.js
+++ client/src/components/FormCreateCompany.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
-import Yup from "yup";
+import * as Yup from "yup";
 import { graphql } from "react-apollo";
 import { addCompany } from "../queries/Queries";
 
@@ -20,9 +20,25 @@ function FormCreateCompany(props) {
   return (
     <div>
       <Form style={{ display: "flex", flexDirection: "column", padding: 30 }}>
-        <Field type="text" name="name" placeholder="Company Name" />
-        <Field type="text" name="address" placeholder="Company Address" />
-        <Field type="text" name="revenue" placeholder="Company Revenue" />
+        <div>
+          {props.touched.name && props.errors.name && (
+            <p>{props.errors.name}</p>
+          )}
+          <Field type="text" name="name" placeholder="Company Name" />
+        </div>
+        <div>
+          {props.touched.address && props.errors.address && (
+            <p>{props.errors.address}</p>
+          )}
+          <Field type="text" name="address" placeholder="Company Address" />
+        </div>
+        <div>
+          {props.touched.revenue && props.errors.revenue && (
+            <p>{props.errors.revenue}</p>
+          )}
+          <Field type="text" name="revenue" placeholder="Company Revenue" />
+        </div>
+
         <div
           style={{
             display: "flex",
@@ -32,8 +48,22 @@ function FormCreateCompany(props) {
         >
           <label for="">Phone Company</label>
           <div>
-            <Field type="text" name="PhoneCode" placeholder="Phone Code" />
-            <Field type="text" name="PhoneNumber" placeholder="Phone Number" />
+            <div>
+              {props.touched.PhoneCode && props.errors.PhoneCode && (
+                <p>{props.errors.PhoneCode}</p>
+              )}
+              <Field type="text" name="PhoneCode" placeholder="Phone Code" />
+            </div>
+            <div>
+              {props.touched.PhoneNumber && props.errors.PhoneNumber && (
+                <p>{props.errors.PhoneNumber}</p>
+              )}
+              <Field
+                type="text"
+                name="PhoneNumber"
+                placeholder="Phone Number"
+              />
+            </div>
           </div>
         </div>
         <button type="submit" onClick={addCompanyMutation}>
@@ -54,6 +84,17 @@ const FormCompany = withFormik({
       PhoneCode: PhoneCode || "",
     };
   },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Company Name must be requiered"),
+    address: Yup.string().required("Company Address must be required"),
+    revenue: Yup.string().required("Revenue must be required"),
+    PhoneNumber: Yup.number()
+      .min(9, "at least 9 character")
+      .required("Phone Number must be requiered"),
+    PhoneCode: Yup.number()
+      .min(2, "at least 2 character")
+      .required("Phone Code must be requiered"),
+  }),
   handleSubmit(values) {
     console.log(values);
   },
